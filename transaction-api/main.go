fd2dbf25a37d15ee24b4d49bb8a649b72d67bd23
Usein Mambediiev <useinm@gmail.com>
2018-05-03 23:23:40 +0300

    added mux to work with http

--- transaction-api/main.go
+++ transaction-api/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	configuration "github.com/usemam/ledger-cloud/transaction-api/configuration"
 )
 
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Test"))
+}
+
 func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", testHandler)
+
 	cfg := configuration.New()
-	go http.ListenAndServe(":"+cfg.Port, nil)
+	log.Fatal(http.ListenAndServe(":"+cfg.Port, router))
 }
