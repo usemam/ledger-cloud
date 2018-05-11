bb87feaed6ea4b2e22b18fe7075603d911dd8198
Usein Mambediiev <useinm@gmail.com>
2018-05-11 10:39:00 +0300

    finished Token parse methods

--- tg-bot/commands/arg-parser/token.go
+++ tg-bot/commands/arg-parser/token.go
@@ -1,12 +1,26 @@
 package parser
 
 import (
+	"fmt"
 	"strconv"
+	str "strings"
+	"time"
+
+	p "github.com/araddon/dateparse"
+	m "github.com/usemam/ledger-cloud/money"
 )
 
 // Token - scanned token
 type Token struct {
 	Value string
+}
+
+type moneyNotParsedError struct {
+	Value string
+}
+
+func (e *moneyNotParsedError) Error() string {
+	return fmt.Sprintf("%q cannot be parsed as Money value.", e.Value)
 }
 
 // Number - gets integer value of token
@@ -17,4 +31,29 @@ func (t *Token) Number() (int, error) {
 // Name - gets string value of token
 func (t *Token) Name() string {
 	return t.Value
+}
+
+// Date - gets date value of token
+func (t *Token) Date() (time.Time, error) {
+	return p.ParseAny(t.Value)
+}
+
+// Money - gets money value of token
+func (t *Token) Money() (m.Money, error) {
+	split := str.Split(t.Value, ".")
+	whole, err := strconv.Atoi(split[0])
+	if err != nil {
+		return m.Zero(), err
+	}
+	if len(split) == 1 {
+		return m.New(uint(whole), 0), nil
+	}
+	if len(split) == 2 {
+		part, err := strconv.Atoi(split[1])
+		if err != nil {
+			return m.Zero(), err
+		}
+		return m.New(uint(whole), uint(part)), nil
+	}
+	return m.Zero(), &moneyNotParsedError{t.Value}
 }
